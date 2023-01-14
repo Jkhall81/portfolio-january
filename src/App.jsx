6551510b37640b262fd0e26ae775e642a36cdb10
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-14 14:55:05 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Sidenav from "./components/Sidenav";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Sidenav />
+    </div>
+  );
+}
+
+export default App;
