cefddb8377c95d9b7e69f4497c89a1ee21cc907f
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-14 19:47:44 -0700

    navbar done

--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -30,28 +30,28 @@ const Sidenav = () => {
             <span className="pl-4">Home</span>
           </a>
           <a
-            hrf="main"
+            hrf="#work"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <GrProjects size={20} />
             <span className="pl-4">Work</span>
           </a>
           <a
-            hrf="main"
+            hrf="#projects"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineProject size={20} />
             <span className="pl-4">Projects</span>
           </a>
           <a
-            hrf="main"
+            hrf="#main"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <BsPerson size={20} />
             <span className="pl-4">Resume</span>
           </a>
           <a
-            hrf="main"
+            hrf="#contact"
             className="w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200"
           >
             <AiOutlineMail size={20} />
@@ -64,31 +64,31 @@ const Sidenav = () => {
       <div className="md:block hidden fixed top-[25%] z-10">
         <div className="flex flex-col">
           <a
-            href="main"
+            href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineHome size={20} />
           </a>
           <a
-            href="main"
+            href="#work"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <GrProjects size={20} />
           </a>
           <a
-            href="main"
+            href="#projects"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineProject size={20} />
           </a>
           <a
-            href="main"
+            href="#main"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <BsPerson size={20} />
           </a>
           <a
-            href="main"
+            href="#contact"
             className="rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300"
           >
             <AiOutlineMail size={20} />
