d569ac67bcb9d901feed04e7d408233867128b80
Jkhall81 <jason.kei.hall@gmail.com>
2023-01-15 10:30:06 -0700

    started work component

---
+++ src/components/Work.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import WorkItem from "./WorkItem";
+
+const data = [
+  {
+    year: 2020,
+    title: "Content Creator",
+    duration: "3 years",
+    details:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, totam voluptates cupiditate obcaecati quidem voluptatem.",
+  },
+  {
+    year: 2017,
+    title: "Google",
+    duration: "3 years",
+    details:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, totam voluptates cupiditate obcaecati quidem voluptatem.",
+  },
+  {
+    year: 2015,
+    title: "Amazon",
+    duration: "2 years",
+    details:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, totam voluptates cupiditate obcaecati quidem voluptatem.",
+  },
+  {
+    year: 2012,
+    title: "Facebook",
+    duration: "3 years",
+    details:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam, totam voluptates cupiditate obcaecati quidem voluptatem.",
+  },
+];
+
+const Work = () => {
+  return <div>Work</div>;
+};
+
+export default Work;
